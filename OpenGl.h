fe2da285f04edf44c3557be51f2cff38e92ff8c8
Byyyrd <105438850+Byyyrd@users.noreply.github.com>
2024-02-21 18:43:52 +0100

    3d huhuu Mouse Callback error

--- OpenGl.h
+++ OpenGl.h
@@ -1,8 +1,18 @@
 #pragma once
 
 #include <filesystem>
+#include <glad/glad.h>
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 class OpenGl
 {
+private:
+	int createWindow(int width, int height, GLFWwindow*& window);
+	
 public: 
+	void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 	int start();
 };
