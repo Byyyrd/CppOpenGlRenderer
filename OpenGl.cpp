2dfa2d6e308773b31eb6380e6e4f5a321e121ec7
Byyyrd <105438850+Byyyrd@users.noreply.github.com>
2024-02-24 14:36:53 +0100

    Normals Fix

--- OpenGl.cpp
+++ OpenGl.cpp
@@ -72,7 +72,7 @@ int OpenGl::start(GLFWwindow* window) {
     -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
-    vertices = float vertices[] = {
+    vertices = {
     -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
      0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
      0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
